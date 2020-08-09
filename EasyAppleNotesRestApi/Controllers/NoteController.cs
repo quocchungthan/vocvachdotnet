bbcfd1ff1ac0eb5e469afdb27dc158117c737771
Than Q. Chung <quocchung.than@gmail.com>
2020-08-09 13:19:08 +0700

    new project for api, may be replaced in the future

---
+++ EasyAppleNotesRestApi/Controllers/NoteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyAppleNotes.ModuleNotes.BusinessLayer.EasyAppleServices;
+using EasyAppleNotes.ModuleNotes.EasyAppleCommonModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAppleNotesRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController: ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Note>> Index()
+        {
+            return _noteService.GetNotes();
+        }
+    }
+}
--- EasyAppleNotesRestApi/Controllers/WeatherForecastController.cs
+++ EasyAppleNotesRestApi/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace EasyAppleNotes.Controllers
+namespace EasyAppleNotesRestApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
