9a34b52bf1e48f884661ca70ce18d80039126b7d
Than Q. Chung <quocchung.than@gmail.com>
2020-10-10 18:36:14 +0700

    flexibility of write file service

--- EasyAppleGenerator/FileIO/FileWritingService.ts
+++ EasyAppleGenerator/FileIO/FileWritingService.ts
@@ -1,23 +1,39 @@
-import * as fs from 'fs';
-import { configService } from '../Configurations/ConfigService';
+import * as fs from "fs";
+import { configService } from "../Configurations/ConfigService";
+import * as path from "path";
 
 class FileWritingService {
-    public async forceWriteFile(fileName: string, fileContent: string): Promise<void> {
-        await this.writeFileAsync(configService.Config.path + fileName, fileContent);
-    }
+  public async forceWriteFile(
+    fileName: string,
+    fileContent: string
+  ): Promise<void> {
+    await this.writeFileAsync(
+      path.join(configService.Config.path, fileName),
+      fileContent
+    );
+  }
 
-    private async writeFileAsync(fileName: string, fileContent: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            fs.writeFile(fileName, fileContent, (err) => {
-                if (!err) {
-                    resolve();
-                } else {
-                    reject(err);
-                }
-            })
-        });
-    }
+  public async forceWriteFileWithAbsolutePath(
+    filePath: string,
+    fileContent: string
+  ): Promise<void> {
+    await this.writeFileAsync(filePath, fileContent);
+  }
 
+  private async writeFileAsync(
+    fileName: string,
+    fileContent: string
+  ): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      fs.writeFile(fileName, fileContent, (err) => {
+        if (!err) {
+          resolve();
+        } else {
+          reject(err);
+        }
+      });
+    });
+  }
 }
 
 export const fileWritingService = new FileWritingService();
