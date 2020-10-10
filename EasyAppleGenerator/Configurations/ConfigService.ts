359a1fe3aa34e08174aaebe28be39240232957bf
Than Q. Chung <quocchung.than@gmail.com>
2020-10-10 13:00:15 +0700

    ignore local config

--- EasyAppleGenerator/Configurations/ConfigService.ts
+++ EasyAppleGenerator/Configurations/ConfigService.ts
@@ -2,21 +2,21 @@ import { findAndReadConfig } from "read-config-file";
 import { IConfiguration } from "./IConfiguration";
 
 class ConfigService {
-    private _config: IConfiguration;
+  private _config: IConfiguration;
 
-    public get Config() {
-        return this._config;
-    }
+  public get Config() {
+    return this._config;
+  }
 
-    public async loadConfigAsync(): Promise<void> {
-        const x = await findAndReadConfig<IConfiguration>({
-            configFilename: "project-configuration",
-            projectDir: "C:/Users/tqchung/source/repos/vocvachdotnet/EasyAppleGenerator/",
-            packageKey: "nsot98er",
-            packageMetadata: null
-        });
-        this._config = x.result;
-    }
+  public async loadConfigAsync(projectDir: string): Promise<void> {
+    const x = await findAndReadConfig<IConfiguration>({
+      configFilename: "project-configuration",
+      projectDir: projectDir,
+      packageKey: "nsot98er",
+      packageMetadata: null,
+    });
+    this._config = x.result;
+  }
 }
 
 export const configService = new ConfigService();
--- EasyAppleGenerator/app.ts
+++ EasyAppleGenerator/app.ts
@@ -2,16 +2,18 @@ import { angularHtmlElementService } from "./AngularsHtml/AngularsHtmlElementSer
 import { configService } from "./Configurations/ConfigService";
 import { fileWritingService } from "./FileIO/FileWritingService";
 
-console.log('This tool provides code generating features: code dotnet & angular forms');
+console.log(
+  "\x1b[33m%s\x1b[0m",
+  "This tool provides code generating features: code dotnet & angular forms"
+);
 
 async function bootstrapping(): Promise<number> {
-    await configService.loadConfigAsync();
-    const input1 = angularHtmlElementService.createInputTextForm();
-    await fileWritingService.forceWriteFile("index.html", input1);
-    return 0;
+  await configService.loadConfigAsync(__dirname);
+  const input1 = angularHtmlElementService.createInputTextForm();
+  await fileWritingService.forceWriteFile("index.html", input1);
+  return 0;
 }
 
-bootstrapping()
-    .then(numberOfErrors => {
-        console.log("Errors: ", numberOfErrors);
-    });
+bootstrapping().then((numberOfErrors) => {
+  console.log("Errors: ", numberOfErrors);
+});
