01f797642ece773704b575e17577e4b6e02f3625
Than Q. Chung <quocchung.than@gmail.com>
2020-10-24 12:34:49 +0700

    detach the access token

---
+++ EasyAppleNotes/ModuleAuthorization/BusinessLayer/ServicesCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyAppleNotes.ModuleAuthorization.BusinessLayer.EasyAppleServices;
+using EasyAppleNotes.ModuleAuthorization.BusinessLayer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace EasyAppleNotes.ModuleAuthorization.BusinessLayer
+{
+    public static class ServicesCollection
+    {
+        // extension method for IServiceCollection objects
+        public static void AddAuthServiceDependency(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationService, AuthorizationService>();
+        }
+
+        public static void UseAuthorizationMiddleware(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<AuthMiddleWare>();
+        }
+    }
+
+    public class AuthMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private readonly IAuthorizationService _authorizationService;
+
+        public AuthMiddleWare(
+            RequestDelegate next,
+            IAuthorizationService authorizationService
+        )
+        {
+            _next = next;
+            _authorizationService = authorizationService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.Headers.TryGetValue(HeaderNames.Authorization, out var authorizations);
+            await _authorizationService.ProcessExtractingAccessTokenAsync(authorizations.FirstOrDefault());
+            await _next(context);
+            return;
+        }
+    }
+}
---
+++ EasyHttpClients/Auth0Client/BaseAuth0Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EasyHttpClients.Auth0Client
+{
+    public class BaseAuth0Client: BaseClient
+    {
+        public BaseAuth0Client():base()
+        {
+        }
+    }
+}
