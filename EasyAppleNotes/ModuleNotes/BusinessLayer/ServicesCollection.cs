a8d22b8aab58714ecf3c031f031c0bfe82638dc6
Chung Than Quoc <chung.thanquoc@axonactive.com>
2021-01-08 10:33:12 +0700

    add comment

--- EasyAppleNotes/ModuleNotes/BusinessLayer/ServicesCollection.cs
+++ EasyAppleNotes/ModuleNotes/BusinessLayer/ServicesCollection.cs
@@ -10,6 +10,14 @@ namespace EasyAppleNotes.ModuleNotes.BusinessLayer
         // extension method for IServiceCollection objects
         public static void AddServiceDependency(this IServiceCollection services)
         {
+            /**
+            can use this to resolve the scoped service issue aswell
+            using (var scope = _serviceProvider.CreateScope()) {
+                var _emailRepository = scope.ServiceProvider.GetRequiredService<IEmailRepository>();
+
+                //do your stuff....
+            }
+            **/
             services.AddSingleton<INoteService, NoteService>();
         }
     }
